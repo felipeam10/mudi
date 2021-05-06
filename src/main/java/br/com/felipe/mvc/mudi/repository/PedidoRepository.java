259aa90f57634d8a93e6a76317b30eee6eb96536
felipeam10 <felipeam10@hotmail.com>
2021-05-06 02:30:10 -0300

    Configurando botões do status: aguardando, aprovado e entregue

--- src/main/java/br/com/felipe/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/br/com/felipe/mvc/mudi/repository/PedidoRepository.java
@@ -1,10 +1,15 @@
 package br.com.felipe.mvc.mudi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.felipe.mvc.mudi.model.Pedido;
+import br.com.felipe.mvc.mudi.model.StatusPedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+
+	List<Pedido> findByStatus(StatusPedido aguardando);
 }
